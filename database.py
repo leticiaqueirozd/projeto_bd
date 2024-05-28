7fa51d093650e9f2b93f514f31e64ac87c90322a
leticiaqueirozd <leticiaqsouza@outlook.com>
2024-05-28 14:47:43 -0300

    update

--- database.py
+++ database.py
@@ -1,1 +1,9 @@
 from connection import DBConnect
+from create import Create
+
+class Database:
+    def __init__(self):
+        conn = DBConnect()
+        c = Create(conn)
+        c.create_db()
+        c.create_tables()
