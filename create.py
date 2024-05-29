221421ec52bc0c526bbb3862cfe8eedc2c86c38d
leticiaqueirozd <leticiaqsouza@outlook.com>
2024-05-29 13:35:22 -0300

    update

--- create.py
+++ create.py
@@ -26,7 +26,7 @@ class Create:
         query = """CREATE TABLE IF NOT EXISTS loja.cliente (
                         id_cliente INT NOT NULL PRIMARY KEY,
                         nome_cliente VARCHAR(50) NOT NULL,
-                        endereco VARCHAR(30) NOT NULL,
+                        endereco VARCHAR(50) NOT NULL,
                         cidade VARCHAR(30) NOT NULL,
                         cep VARCHAR(9) NOT NULL,
                         UF VARCHAR(2) NOT NULL
@@ -35,40 +35,40 @@ class Create:
 
     def create_table_vendedor(self):
         query = """CREATE TABLE IF NOT EXISTS loja.vendedor (
-                                id_vendedor INT NOT NULL PRIMARY KEY,
-                                nome_vendedor VARCHAR(50) NOT NULL,
-                                salario_fixo INT NOT NULL,
-                                faixa_comissao INT
-                            );"""
+                        id_vendedor INT NOT NULL PRIMARY KEY,
+                        nome_vendedor VARCHAR(50) NOT NULL,
+                        salario_fixo INT NOT NULL,
+                        faixa_comissao INT
+                    );"""
         self.execute_query(query)
 
     def create_table_pedido(self):
         query = """CREATE TABLE IF NOT EXISTS loja.pedido (
-                                        id_pedido INT NOT NULL PRIMARY KEY,
-                                        prazo_entrega DATE NOT NULL,
-                                        id_vendedor INT NOT NULL,
-                                        id_cliente INT NOT NULL,
-                                        FOREIGN KEY (id_vendedor) REFERENCES vendedor(id_vendedor),
-                                        FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
-                                    );"""
+                        id_pedido INT NOT NULL PRIMARY KEY,
+                        prazo_entrega DATE NOT NULL,
+                        id_vendedor INT NOT NULL,
+                        id_cliente INT NOT NULL,
+                        FOREIGN KEY (id_vendedor) REFERENCES vendedor(id_vendedor),
+                        FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+                    );"""
         self.execute_query(query)
 
     def create_table_produto(self):
         query = """CREATE TABLE IF NOT EXISTS loja.produto (
-                                                id_produto INT NOT NULL PRIMARY KEY,
-                                                unidade INT NOT NULL,
-                                                descricao VARCHAR(50),
-                                                valor FLOAT NOT NULL
-                                            );"""
+                        id_produto INT NOT NULL PRIMARY KEY,
+                        unidade INT NOT NULL,
+                        descricao VARCHAR(50),
+                        valor FLOAT NOT NULL
+                    );"""
         self.execute_query(query)
 
     def create_table_item_pedido(self):
         query = """CREATE TABLE IF NOT EXISTS loja.item_pedido (
-                                                id_produto INT NOT NULL,
-                                                id_pedido INT NOT NULL,
-                                                quantidade INT NOT NULL,
-                                                PRIMARY KEY (id_produto, id_pedido)
-                                            );"""
+                        id_produto INT NOT NULL,
+                        id_pedido INT NOT NULL,
+                        quantidade INT NOT NULL,
+                        PRIMARY KEY (id_produto, id_pedido)
+                    );"""
         self.execute_query(query)
 
     def create_tables(self):
